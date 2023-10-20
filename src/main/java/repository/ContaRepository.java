c2c4c84e7c54ef6a5b67f0da939d63ac00dc8e24
Talita Maria <TalitaMaria@Litas-MacBook-Pro.local>
2023-10-20 12:16:54 -0300

    classes transfer, conta e extrato

---
+++ src/main/java/repository/ContaRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import entity.Conta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContaRepository extends JpaRepository<Conta, Long> {
+
+    boolean existsByConta(long conta);
+}
